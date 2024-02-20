d76898f20a1bd017363133fb674c939aa4433165
IvanJelicSF <ivan.jelic@sourcefabric.org>
2024-02-20 13:00:49 +0100

    Publisher on

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -9,14 +9,32 @@ module.exports = function() {
         apps: [
             'superdesk.analytics',
             'superdesk-planning',
+            'superdesk-publisher'
         ],
         importApps: [
             '../index',
             'superdesk-planning',
             'superdesk-analytics',
+            'superdesk-publisher'
         ],
 
         defaultRoute: '/workspace/monitoring',
+
+         publisher: {
+            protocol: 'https',                /* http or https */
+            tenant: '',              /* tenant - semantically subdomain, '' is allowed */
+            domain: '',           /* domain name for the publisher */
+            base: 'api/v2',                  /* api base path */
+
+            wsProtocol: 'wss',                /* ws or wss (websocket); if unspecified or '' defaults to 'wss' */
+            wsDomain: '',  /* domain name (usually domain as above) */
+                                            /* e.g.: example.com, abc.example.com */
+                                            /* tenant, as above, is NOT used for websocket */
+            wsPath: '/ws',                    /* path to websocket root dir */
+            wsPort: '80',                   /* if not specified: defaults to 443 for wss, 80 for ws */
+            hideContentRoutesInPublishPane: false, /* hides routes of type "content" from select box in publish panes in monitoring view as well as in output control. If not specified: defaults to false */
+            hideCustomRoutesInPublishPane: false   /* hides routes of type "custom" from select box in publish panes in monitoring view as well as in output control. If not specified: defaults to false */
+        },
 
         langOverride: {
             en: {
